0e23fbba978d556877da5a53c9e0a0f6337a4689
jlipes <jason.lipes@gmail.com>
2015-07-13 22:42:40 -0500

    minor change

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -88,7 +88,7 @@ class CalculationsController < ApplicationController
 
     @mean = @numbers.inject{|sum,x| sum + x }/@count
 
-    @variance = @numbers.inject{|sum,x| sum+(x-@mean)**2}/9
+    @variance = @numbers.inject{|sum,x| sum+(x-@mean)**2}/@numbers.count
 
     @standard_deviation = @variance**0.5
 
